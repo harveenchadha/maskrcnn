be0975f48cbcdd83ee2f0bd87ffdbb9addb4c7d0
harveen54@gmail.com <harveen54@gmail.com>
2018-11-29 14:18:53 +0000

    App.py

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask
-import t
+
+from garbage_det import * 
 
 app = Flask(__name__)
+from flask import Flask, jsonify, make_response
+from flask import request
 
 @app.route('/garbage/<path>')
 def index(path):
@@ -11,13 +14,20 @@ def index(path):
 
 @app.route('/garbage/analyze', methods = ['POST'])
 def analyze_aisle_image():
-   print('request.args ', request.form)
-   ais_img=request.form.get('AIS_IMG', default='./ais_repo/ais1.png', type=str)
-   response_text = process_pipeline(ais_img, brand, item)
-   resp = make_response(json.dumps(response_text))
-   resp.headers['Access-Control-Allow-Origin'] = '*'
-   return resp
+  #print('request.args ', request.form)
+  print(request.files)
+  target = 'images'
+  file = request.files['cssv_file'] 
+  file.save('./images/test.jpg')
+  #print(os.system("python3 garbage_det.py detect --weights='./mask_rcnn_garbage_0010.h5' --image='test.jpg'"))
+        
+  ret_value = detect_and_save("hi", image_path='./images/test.jpg')
+  if(ret_value):
+    return "True"
+  else:
+    return "False"
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=8080)
     app.run(debug=True)
